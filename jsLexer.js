1c6d17c3d6d84b7aa0de46207d94685fbb4e8a6e
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-12-29 11:13:35 +0530

    foundations

---
+++ jsLexer.js
@@ -0,0 +1,22 @@
+const rules = [
+    { pattern: "/\S/g", name: "whitespace" },
+    { pattern: /^\d+/, name: "digits" },
+    { pattern: /^[a-zA-Z_]\w*/, name: "identifier" },
+    { pattern: /^\+/, name: "plus" },
+    { pattern: /^\-/, name: "minus" },
+    { pattern: /^\*/, name: "multiply" },
+    { pattern: /^\\/, name: "divide" },
+    { pattern: /^\(/, name: "openbrace" },
+    { pattern: /^\)/, name: "closebrace" },
+    { pattern: /^=/, name: "equal" },
+]
+
+let pos = 0;
+const buf = "for ( const i = 0; ) ";
+const tokens = [];
+
+while (pos < buf.length) {
+    for (let i = 0; i < rules.length; i++){
+        
+    }
+}
