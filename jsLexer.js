bee29447fd512b054c599887239f09db10d542c7
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-12-29 12:24:08 +0530

    Done! Whitespace is now ignored

--- jsLexer.js
+++ jsLexer.js
@@ -1,10 +1,14 @@
 const rules = [
-    { pattern: "^ +|\t|\n/g", name: "whitespace" },
+    { pattern: /^ +|\t|\n/g, name: "whitespace" },
     { pattern: /^\d+/, name: "digits" },
-    { pattern: /^[a-zA-Z_]\w*/, name: "identifier" },
-    { pattern: /^\+|\*|\/|\-/, name: "operator" },
-    { pattern: /^\(/, name: "openbrace" },
-    { pattern: /^\)/, name: "closebrace" },
+    { pattern: /^[a-zA-Z_]\w*/, name: "Identifiers" },
+    { pattern: /^\+|\*|\/|\-/, name: "Operators" },
+    { pattern: /^\(/, name: "openBracket" },
+    { pattern: /^\)/, name: "closeBracket" },
+    { pattern: /^\{/, name: "openBraces" },
+    { pattern: /^\}/, name: "closeBraces" },
+    { pattern: /^\{/, name: "openSquare" },
+    { pattern: /^\}/, name: "closeSquare" },
     { pattern: /^=/, name: "equal" },
     { pattern: /^;/, name: "semicolon" }
 ]
@@ -16,10 +20,10 @@ const buffer = "for(const i=0;)";
 
 const tokens = [];
 
-let rule = new RegExp(rules[i].pattern);
 
 while (pos < buffer.length) {
     for (let i = 0; i < rules.length; i++) {
+        let rule = rules[i].pattern;
         const match = rule.exec(buffer.substr(pos))
         if (match) {
 
@@ -29,11 +33,12 @@ while (pos < buffer.length) {
                 continue;
             }
 
-            // Testing
-            console.log(match[0])
-            
             // Increment Postion
             pos += match[0].length;
+
+            tokens.push(match[0])
         }
     }
 }
+
+console.log(tokens)
