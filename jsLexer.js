542c3840eac25a26060a257facfbd55c4159a059
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-12-29 11:28:43 +0530

    working except for whitespaces

--- jsLexer.js
+++ jsLexer.js
@@ -1,19 +1,29 @@
 const rules = [
-    { pattern: "/\S/g", name: "whitespace" },
+    { pattern: "^ +|\t|\n/g", name: "whitespace" },
     { pattern: /^\d+/, name: "digits" },
     { pattern: /^[a-zA-Z_]\w*/, name: "identifier" },
     { pattern: /^\+|\*|\/|\-/, name: "operator" },
     { pattern: /^\(/, name: "openbrace" },
     { pattern: /^\)/, name: "closebrace" },
     { pattern: /^=/, name: "equal" },
+    { pattern: /^;/, name: "semicolon" }
 ]
 
 let pos = 0;
-const buf = "for ( const i = 0; ) ";
+const buffer = "for(const i=0;)";
 const tokens = [];
 
-while (pos < buf.length) {
-    for (let i = 0; i < rules.length; i++){
-        
+while (pos < buffer.length) {
+    for (let i = 0; i < rules.length; i++) {
+        let rule = new RegExp(rules[i].pattern);
+        const match = rule.exec(buffer.substr(pos))
+        if (match) {
+            if (i == 0) {
+                pos += match.index;
+                continue;
+            }
+            console.log(match[0])
+            pos += match[0].length;
+        }
     }
 }
