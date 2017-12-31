681a5730a567d9b0380f85c50912d793b393a9d1
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-12-31 23:26:39 +0530

    Tokenizer tests: Passing

--- tests/lexer.js
+++ tests/lexer.js
@@ -5,8 +5,9 @@ const LexerJS = new Lexer();
 
 describe('LexerJS - Tokenizer Check', () => {
     it('should find all tokens', () => {
-        expect(LexerJS.tokenizer("const i = 0; i++;").length).equal(8);
+        expect(LexerJS.tokenizer("a += c;").length).equal(5);        
         expect(LexerJS.tokenizer("for int i = 0;").length).equal(6);
+        expect(LexerJS.tokenizer("const i = 0; i++;").length).equal(8);
         expect(LexerJS.tokenizer("for int > i = 0;").length).equal(7);
         expect(LexerJS.tokenizer("for int i = 0; \n i--;").length).equal(9);
     });
@@ -28,7 +29,6 @@ describe('LexerJS - Tokenizer Check', () => {
         for(int i = 0;)
         `);
 
-        // console.log(tokens);
         expect(tokens.length).equal(18);
     });
 })
